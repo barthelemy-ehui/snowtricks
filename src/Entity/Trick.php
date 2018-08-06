91743e492a85d28c9889484ae4ede808a6c23ded
Barthelemy <barthelemy.ehui@gmail.com>
2018-08-06 14:25:03 +0200

    update trick entity

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -59,6 +59,11 @@ class Trick
      */
     private $comments;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
     public function __construct()
     {
         $this->comments = new ArrayCollection();
@@ -187,6 +192,18 @@ class Trick
                 $comment->setTrick(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }

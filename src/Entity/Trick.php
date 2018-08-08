7687d5859708e70b50288d5eeca03597645aec96
Barthelemy <barthelemy.ehui@gmail.com>
2018-08-08 09:43:58 +0200

    add trick with comment

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TrickRepository")
@@ -20,6 +21,7 @@ class Trick
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $name;
 
@@ -200,6 +202,14 @@ class Trick
         $this->resource = $resource;
 
         return $this;
+    }
+    
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
     }
     
 }

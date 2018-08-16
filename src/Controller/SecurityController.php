6e13872b6d92ec29a828c891c6a9336a4a84f482
Barthelemy <barthelemy.ehui@gmail.com>
2018-08-16 20:42:47 +0200

    add login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,15 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bart
- * Date: 16/08/2018
- * Time: 12:35
- */
-
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController
+class SecurityController extends AbstractController
 {
-    
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        
+        $lastUserName = $authenticationUtils->getLastUsername();
+        
+        return $this->render('security/login.html.twig', [
+           'last_username' => $lastUserName,
+           'error' => $error
+        ]);
+    }
 }
